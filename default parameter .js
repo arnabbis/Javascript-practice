73b519acaa5e0fb2910bb827440eac345beca4fa
arnabbiswas1 <arnab.biswas1@olacabs.com>
2022-07-12 12:53:49 +0530

    default,higherOrder,prototype

---
+++ default parameter .js
@@ -0,0 +1,29 @@
+
+// function x(a,b=4,c=5){
+//     //let a = 4;
+//    // console.log(a+b);
+//    return a+b*c
+// }
+
+// let res = x(7,6,7)
+// console.log(res)
+
+// let arnab = function(c,b=false,a=5){
+//     let d = 10;
+//     if(b==true || typeof b === 'string'){
+//         return d+c*a
+//     }else{
+//         return d+1;
+//     }
+// }
+
+// let res = arnab(34,7)
+// console.log(res)
+
+// let nw = "KOmal";
+// console.log(typeof nw);
+
+ 
+
+
+
